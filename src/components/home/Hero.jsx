4072875680548c10fcdecd4780174791ffb859ad
rifkiafnt12 <rifkiafnt12@gmail.com>
2022-10-17 13:43:52 +0700

    button google play

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -13,7 +13,14 @@ export const Hero = () => {
               <Typewriter options={{ strings: [`${val.name}`, `${val.post}`, `${val.design}`, `${val.surat}`, `${val.fitur}`], autoStart: true, loop: true }} />
             </h1>
             <p data-aos="fade-left">{val.desc}</p>
-            <button data-aos="fade-up-right">Download in PlayStore & AppStore</button>
+            <div className="platform">
+              <a href="https://play.google.com/store/apps/details?id=com.QuranIndo.App">
+                <img src="https://quranforandroid.com/assets/img/badge-googleplay-id.png" alt="" data-aos="fade-up-right" />
+              </a>
+              <a href="https://itunes.apple.com/id/app/id1475973021">
+                <img src="https://quranforandroid.com/assets/img/badge-appstore-id.png" alt="" data-aos="fade-up-left" />
+              </a>
+            </div>
           </div>
         ))}
       </section>
